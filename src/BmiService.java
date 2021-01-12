cd609811f7356353279836b2e3984d0c52e2bdf6
Cook1eM <73870450+Cook1eM@users.noreply.github.com>
2021-01-12 06:01:15 +0300

    Delete src directory

--- src/BmiService.java
+++
@@ -1,8 +0,0 @@
-
-public class BmiService {
-     public float calculate (float growth, float weight){
-        float BodyMassindex = weight /( growth * growth);
-         return BodyMassindex;
-     }
-
-}
--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BmiService service = new BmiService();
-        float weight = 94;
-        float growth = 184;
-        float BodyMassindex = service.calculate( growth, weight);
-        System.out.println(BodyMassindex);
-    }
-}
